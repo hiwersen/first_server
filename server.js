c88e57aa62f1788f67a5de4e3207fe981d435b8d
Hiwersen Angelo Gnocchi Godoy <100746220+hiwersen@users.noreply.github.com>
2023-04-21 15:33:17 +0100

    server updated

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ let app = express(); // returns express object
 let bodyParser = require('body-parser');
 require('dotenv').config();
 
+app.use((req, res, next) => {
+    let { method, path, ip } = req;
+    console.log(`${method} ${path} - ${ip}`);
+    next(); // pass control to the next function
+})
+
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
